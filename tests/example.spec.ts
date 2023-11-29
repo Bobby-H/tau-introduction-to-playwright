a560096c8a20445c164c7ac0c609ca7a4e483946
Bobby Hutter <bobbyhutter@Bobbys-MacBook-Pro.local>
2023-11-29 05:19:21 -0500

    wrote my first test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,13 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+/** 
+ * 1. Open the page
+ * 2. Click at Get Started
+ * 3. Mouse hover the language dropdown
+ * 4. Click Java
+ * 5. Check the URL
+ * 6. Check the text "Installing Playwright" is not being displayed
+ * 7. Check the text below is displayed
+ */
